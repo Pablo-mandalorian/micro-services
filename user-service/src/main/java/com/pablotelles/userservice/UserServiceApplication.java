fc9e29c8c2e32681a6fe42b6ad7ea2018b5b598e
Pablo-mandalorian <pablotelles935@gmail.com>
2023-04-25 14:15:39 -0600

    Added Eureka Service

--- user-service/src/main/java/com/pablotelles/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/pablotelles/userservice/UserServiceApplication.java
@@ -2,11 +2,13 @@ package com.pablotelles.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 //Agregar OpenFeign Al pom y despues agregar @enable.... al main 
 @EnableFeignClients
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
