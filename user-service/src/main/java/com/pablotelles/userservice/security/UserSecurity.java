6754bf716c65a3cfa44b706cb6bd1ab4c12f0594
Pablo-mandalorian <pablotelles935@gmail.com>
2023-05-03 15:39:36 -0600

    Keycloack

---
+++ user-service/src/main/java/com/pablotelles/userservice/security/UserSecurity.java
@@ -0,0 +1,10 @@
+package com.pablotelles.userservice.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class UserSecurity{
+   
+}
